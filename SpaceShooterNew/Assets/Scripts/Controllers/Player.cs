acf5c4593818f6dab50fd5bb6685b47f9f487a46
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2024-09-18 11:56:14 -0400

    Task 1: Made player move with keyboard input

--- SpaceShooterNew/Assets/Scripts/Controllers/Player.cs
+++ SpaceShooterNew/Assets/Scripts/Controllers/Player.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+
     public List<Transform> asteroidTransforms;
     public Transform enemyTransform;
     public GameObject bombPrefab;
@@ -11,7 +14,45 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        //This is a change
+        Vector2 input = new Vector2();
+
+        // input.x = -Convert.ToByte(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) + Convert.ToByte(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D));
+        // input.y = Convert.ToByte(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) - Convert.ToByte(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S));
+
+        switch (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            case (true, false):
+                input.x = -1;
+                break;
+
+            case (false, true):
+                input.x = 1;
+                break;
+
+            default:
+                input.x = 0;
+                break;
+        }
+        switch (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            case (true, false):
+                input.y = 1;
+                break;
+
+            case (false, true):
+                input.y = -1;
+                break;
+
+            default:
+                input.y = 0;
+                break;
+        }
+
+        PlayerMovement(input);
     }
 
+    private void PlayerMovement(Vector2 inputVector)
+    {
+        transform.position += (Vector3)inputVector.normalized * speed * Time.deltaTime;
+    }
 }
