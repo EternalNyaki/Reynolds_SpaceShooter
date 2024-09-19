d6ef37ac1d9d3ec728c47b38017a7486cdafc532
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2024-09-19 19:14:46 -0400

    Task 2: Made enemy movement

--- SpaceShooterNew/Assets/Scripts/Controllers/Enemy.cs
+++ SpaceShooterNew/Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,32 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
-    private void Update()
+    public float speed;
+
+    private Vector2 target;
+
+    public Transform player;
+
+    private void Start()
     {
+        target = new Vector2(player.position.x, player.position.y);
     }
 
+    private void Update()
+    {
+        Vector2 toTarget = target - (Vector2)transform.position;
+        Vector2 deltaPos = toTarget.normalized * speed * Time.deltaTime;
+
+        if (deltaPos.magnitude >= toTarget.magnitude)
+        {
+            transform.position = target;
+            target = new Vector2(player.position.x, player.position.y);
+        }
+        else
+        {
+            transform.position += (Vector3)deltaPos;
+        }
+
+        Debug.DrawLine(transform.position, target, Color.green);
+    }
 }
