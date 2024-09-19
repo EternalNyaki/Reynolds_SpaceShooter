2f955eeac34bb5466ef2b4100c63f509e6b58219
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2024-09-19 19:57:12 -0400

    Task 4: Drawing a constellation

--- SpaceShooterNew/Assets/Scripts/Controllers/Stars.cs
+++ SpaceShooterNew/Assets/Scripts/Controllers/Stars.cs
@@ -7,8 +7,28 @@ public class Stars : MonoBehaviour
     public List<Transform> starTransforms;
     public float drawingTime;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        StartCoroutine(DrawStars());
+    }
+
+    private IEnumerator DrawStars()
+    {
+        for (int i = 0; i < starTransforms.Count - 1; i++)
+        {
+            float timer = 0;
+            while (timer < drawingTime)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Debug.DrawLine(starTransforms[j].position, starTransforms[j + 1].position, Color.white);
+                }
+                Debug.DrawLine(starTransforms[i].position, Vector3.Lerp(starTransforms[i].position, starTransforms[i + 1].position, timer / drawingTime), Color.white);
+
+                timer += Time.deltaTime;
+
+                yield return null;
+            }
+        }
     }
 }
