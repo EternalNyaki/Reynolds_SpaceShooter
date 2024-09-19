78d680e74e527d902c9deb49628b5f610a909e26
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2024-09-19 18:55:48 -0400

    Tasks 1B and 1C: Make the player accelerate and decelerate smoothly

--- SpaceShooterNew/Assets/Scripts/Controllers/Player.cs
+++ SpaceShooterNew/Assets/Scripts/Controllers/Player.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    public float maxSpeed;
+    public float accelerationTime;
+    public float decelerationTime;
+
+    private float acceleration;
+    private float deceleration;
+    private Vector2 velocity;
 
     public List<Transform> asteroidTransforms;
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    void Start()
+    {
+        acceleration = maxSpeed / accelerationTime;
+        deceleration = maxSpeed / decelerationTime;
+    }
+
     void Update()
     {
         Vector2 input = new Vector2();
 
+        //Fun alt code for getting input (~0.01ms/frame faster)
         // input.x = -Convert.ToByte(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) + Convert.ToByte(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D));
         // input.y = Convert.ToByte(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) - Convert.ToByte(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S));
 
@@ -48,11 +61,47 @@ public class Player : MonoBehaviour
                 break;
         }
 
-        PlayerMovement(input);
+        PlayerMovement(input.normalized);
     }
 
     private void PlayerMovement(Vector2 inputVector)
     {
-        transform.position += (Vector3)inputVector.normalized * speed * Time.deltaTime;
+        float tempDecel = deceleration * Time.deltaTime;
+        if (inputVector.x == 0)
+        {
+            if (tempDecel >= Mathf.Abs(velocity.x))
+            {
+                velocity.x = 0;
+            }
+            else
+            {
+                velocity.x -= Mathf.Sign(velocity.x) * tempDecel;
+            }
+        }
+        else
+        {
+            velocity.x += inputVector.x * acceleration * Time.deltaTime;
+        }
+
+        if (inputVector.y == 0)
+        {
+            if (tempDecel >= Mathf.Abs(velocity.y))
+            {
+                velocity.y = 0;
+            }
+            else
+            {
+                velocity.y -= Mathf.Sign(velocity.y) * tempDecel;
+            }
+        }
+        else
+        {
+            velocity.y += inputVector.y * acceleration * Time.deltaTime;
+        }
+
+        velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+        velocity.y = Mathf.Clamp(velocity.y, -maxSpeed, maxSpeed);
+
+        transform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
