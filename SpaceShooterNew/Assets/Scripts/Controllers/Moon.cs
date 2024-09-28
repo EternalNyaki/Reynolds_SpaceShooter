e2432ec883857c21dff7d00e4ca8fcca7c7e9f5e
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2024-09-27 23:32:43 -0400

    Week 4 task 3: Added orbital system

--- SpaceShooterNew/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooterNew/Assets/Scripts/Controllers/Moon.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Moon : MonoBehaviour
 {
+    public float orbitalRadius;
+    public float orbitalSpeed;
+
+    private float angle = 0;
+
     public Transform planetTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * orbitalRadius + planetTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(orbitalRadius, orbitalSpeed, planetTransform);
+    }
 
+    private void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        angle += speed * Time.deltaTime;
+        angle %= 360;
+        transform.position = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * radius + target.position;
     }
 }
